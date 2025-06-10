5af4b523d28d6a6f1d5d935df288420ce9dd0030
Akash <aircracing.in@gmail.com>
2025-06-10 20:48:46 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ import { ThemeProvider } from "@/components/common/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
-import { ModalProvider } from "@/providers/modal-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -78,7 +77,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
         >
           {children}
           <Toaster />
-          <ModalProvider />
         </ThemeProvider>
       </body>
     
--- config/routes.ts
+++ config/routes.ts
@@ -7,10 +7,6 @@ export const routesConfig: any = {
     {
       title: "Deep Dive",
       href: "/details",
-    },
-    {
-      title: "Contact",
-      href: "/contact",
-    },
+    }
   ],
 };
