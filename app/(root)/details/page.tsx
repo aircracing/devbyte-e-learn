2b6fbd2db8abba4793e8f0e8628eb2c47936f528
Akash <aircracing.in@gmail.com>
2025-06-11 22:20:16 +0530

    update

--- app/(root)/details/page.tsx
+++ app/(root)/details/page.tsx
@@ -31,7 +31,7 @@ let expArr: any = [];
 
   return (
     <div className="mx-auto my-4 grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3 static">
-      {expArr.map((exp) => (
+      {expArr.map((exp:any) => (
         <ProjectCard project={exp} key={exp.id} />
       ))}
     </div>
